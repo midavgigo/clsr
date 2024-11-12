150b5421d9712607781de2866e38de30430b8df0
GUL415D <ddd20162004@gmail.com>
2024-11-12 12:44:41 +0300

    раотает

--- projects/server/src/main.cpp
+++ projects/server/src/main.cpp
@@ -8,6 +8,32 @@
 #include <fstream>
 #include <stdlib.h>
 
+void* proc_task(void *task_ptr){
+    task_t task = *(task_t *)task_ptr;
+    for(cmd_arg_t cmd : task){
+        switch(cmd.first){
+            case Command::Write:{
+                    char path[256];
+                    char text[256];
+                    cmd.second[0].getValue(path);
+                    cmd.second[1].getValue(text);
+                    Logger::logf("Path %s Text %s", path, text);
+                    std::ofstream fout(path);
+                    fout << text;
+                    fout.close();
+                    break;
+                }
+            case Command::Execute:{
+                    char command[256];
+                    cmd.second[0].getValue(command);
+                    system(command);
+                    break;
+                }
+        }
+    }
+    return nullptr;
+}
+
 int main(){
     Logger::init("SERVER");
     Logger::logf("Server's logger initialized");
@@ -19,6 +45,8 @@ int main(){
         Request r;
         if(r.parse(std::move(buffer))){
             //tq.addCommand(r.getCommands());
+            task_t tasks = r.getCommands();
+            proc_task(&tasks);
             for(cmd_arg_t cmd : r.getCommands()){
                 Logger::logf("Getted command: %d", cmd.first);
                 for(Arg a : cmd.second){
--- projects/server/src/threading/TaskQueue.cpp
+++ projects/server/src/threading/TaskQueue.cpp
@@ -4,31 +4,31 @@ size_t TaskQueue::addCommand(task_t task){
     q_.push(task);
 }
 
-void* proc_task(void *task_ptr){
-    task_t task = *(task_t *)task_ptr;
-    for(cmd_arg_t cmd : task){
-        switch(cmd.first){
-            case Command::Write:{
-                    char path[256];
-                    char text[256];
-                    cmd.second[0].getValue(path);
-                    cmd.second[1].getValue(text);
-                    Logger::logf("Path %s Text %s", path, text);
-                    std::ofstream fout(path);
-                    fout << text;
-                    fout.close();
-                    break;
-                }
-            case Command::Execute:{
-                    char command[256];
-                    cmd.second[0].getValue(command);
-                    system(command);
-                    break;
-                }
-        }
-    }
-    return nullptr;
-}
+// void* proc_task(void *task_ptr){
+//     task_t task = *(task_t *)task_ptr;
+//     for(cmd_arg_t cmd : task){
+//         switch(cmd.first){
+//             case Command::Write:{
+//                     char path[256];
+//                     char text[256];
+//                     cmd.second[0].getValue(path);
+//                     cmd.second[1].getValue(text);
+//                     Logger::logf("Path %s Text %s", path, text);
+//                     std::ofstream fout(path);
+//                     fout << text;
+//                     fout.close();
+//                     break;
+//                 }
+//             case Command::Execute:{
+//                     char command[256];
+//                     cmd.second[0].getValue(command);
+//                     system(command);
+//                     break;
+//                 }
+//         }
+//     }
+//     return nullptr;
+// }
 
 void TaskQueue::proc(){
     if(q_.size() <= 0){
@@ -40,7 +40,7 @@ void TaskQueue::proc(){
             Logger::logf("Thread %d is free", i);
             task_t task = q_.front();
             q_.pop();
-            pthread_create(&threads_[i], nullptr, proc_task, &task);
+            //pthread_create(&threads_[i], nullptr, proc_task, &task);
         }
     }
 }
